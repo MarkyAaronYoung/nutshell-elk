7a1c431fccf3bf41521f36bcc14b1cd41912475f
Billy Campbell <wcampbell91@gmail.com>
2020-07-16 19:01:42 -0500

    WIP moving souvs to souvBuilder.js

---
+++ src/javascripts/components/souvBuilder/souvBuilder.js
@@ -0,0 +1,33 @@
+const souvBuilder = (souv) => {
+  let domString = '';
+  if (souv.isAvailable === true) {
+    domString = `
+                  <div class="card text-center souv-card" id="${souv.id}" style="width: 18rem;">
+                    <img class="card-img-top" id="souv-image" src="${souv.imageUrl}" alt="...">
+                    <div class="card-body">
+                      <h3 class="card-title">${souv.name}</h3>
+                      <h4>${souv.price}</h4>
+                      <p>${souv.description}</p>
+                      <button class="btn btn-primary auth-button" id="edit-souv">Edit</button>
+                      <button class="btn btn-primary auth-button" id="delete-souv">Delete</button>
+                    </div>
+                  </div>`;
+  } else {
+    domString = `
+                  <div class="card text-center souv-card notAvail" id="${souv.id}" style="width: 18rem;">
+                    <img class="card-img-top" id="souv-image" src="${souv.imageUrl}" alt="...">
+                    <div class="card-body">
+                      <h3 class="card-title">${souv.name}</h3>
+                      <h4>${souv.price}</h4>
+                      <p>${souv.description}</p>
+                      <p>SOLD OUT!</P>
+                      <button class="btn btn-primary auth-button" id="edit-souv">Edit</button>
+                      <button class="btn btn-primary auth-button" id="delete-souv">Delete</button>
+                    </div>
+                  </div>`;
+  }
+
+  return domString;
+};
+
+export default { souvBuilder };
--- src/javascripts/components/souvList/souvList.js
+++ src/javascripts/components/souvList/souvList.js
@@ -10,23 +10,15 @@ const souvBuilder = (e) => {
   souvData.getSouv()
     .then((souvs) => {
       let domString = `
-                      <h2 class="text-center">souvenirs</h2>
+                    <div class="text-center">
+                      <h2 class="text-center">Souvenirs</h2>
+                      <button class="btn btn-primary auth-button text-center" id="add-souv">Add New Souvenir</button>
+                    </div>
                       <div class="display-flex flex-wrap mySouvenirs card-deck">`;
       souvs.forEach((souv) => {
-        if (souv.isAvailable === true) {
-          domString += `
-                        <div class="card text-center souv-card" id="${souv.id}" style="width: 18rem;">
-                          <img class="card-img-top" id="souv-image" src="${souv.imageUrl}" alt="...">
-                          <div class="card-body">
-                            <h3 class="card-title">${souv.name}</h3>
-                            <h4>${souv.price}</h4>
-                            <p>${souv.description}</p>
-                            <button class="btn btn-primary auth-button" id="edit-souv">Edit</button>
-                            <button class="btn btn-secondary auth-button" id="add-souv">Add</button>
-                          </div>
-                        </div>`;
-        }
+        domString += souvBuilder.souvBuilder(souv);
       });
+
       domString += '</div>';
       utils.printToDom('#souvenirs', domString);
     })
