e473ce2d28a9eec30b2362c48285dc1996c95160
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-08-02 16:16:56 -0500

    Added Edit and Delete Buttons

--- src/javascripts/components/eventsList/eventsList.js
+++ src/javascripts/components/eventsList/eventsList.js
@@ -17,7 +17,6 @@ const viewIndividualEvent = (e) => {
   smash.getSingleEventInfo(eventId)
     .then((event) => {
       const domString = `
-<<<<<<< Updated upstream
       <h2 class=>${event.name}</h2>
       <div id="app">
        <div id="food" class="quad">
@@ -104,105 +103,6 @@ const viewIndividualEvent = (e) => {
       console.warn('This does work!', event);
       utils.printToDom('#individual-event', domString);
     });
-=======
-                          <h2 class=>${event.name}</h2>
-                          <div class="card-deck food-staff-container">
-                            <div id="food" class="quad">
-                              <table class='table table-bordered'>
-                              <thead class ="colored">
-                                <tr>
-                                  <th>Food</th>
-                                  <th>Quantity</th>
-                                </tr>
-                                </thead>
-                                <tr>
-                                  <td>${event.food}</td>
-                                  <td><input type="text"></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                              </table>
-                            </div>
-                            <div id="staff" class="quad">
-                              <table class='table table-bordered'>
-                              <thead class ="colored">
-                                <tr>
-                                  <th>Staff</th>
-                                  <th>Quantity</th>
-                                </tr>
-                                </thead>
-                                <tr>
-                                  <td>${event.staff}</td>
-                                  <td><input type="text"></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                              </table>
-                            </div>
-                          </div>
-                          <div class="card-deck show-souv-container">
-                            <div id="show" class="quad">
-                              <table class='table table-bordered'>
-                              <thead class ="colored">
-                                <tr>
-                                  <th>Show</th>
-                                  <th>Quantity</th>
-                                </tr>
-                                </thead>
-                                <tr>
-                                  <td>${event.show.name}</td>
-                                  <td><input type="text"></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                              </table>
-                            </div>
-                            <div id="Souvenirs" class="quad">
-                              <table class='table table-bordered'>
-                              <thead class ="colored">
-                                <tr>
-                                  <th>Souvenirs</th>
-                                  <th>Quantity</th>
-                                </tr>
-                                </thead>
-                                <tr>
-                                  <td>${event.souv}</td>
-                                  <td><input type="text"></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                                <tr>
-                                  <td></td>
-                                  <td></td>
-                                </tr>
-                              </table>
-                            </div>
-                          </div>`;
-      console.warn(event);
-      utils.printToDom('#individual-event', domString);
-    })
-    .catch((err) => console.error('It broke', err));
->>>>>>> Stashed changes
 
   $('#events-page').addClass('hide');
   $('#individual-event').removeClass('hide');
